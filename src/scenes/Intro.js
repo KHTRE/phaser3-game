6539778a8e4132ef29096358f34ccc03bf7a3e11
KHTRE <KHTREE@gmail.com>
2022-02-20 16:05:45 +0200

    promises

--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -37,11 +37,10 @@ export class Intro extends Phaser.Scene {
       element.setScale(initScale * heightScale);
     }
 
-    function setAdaptivePosition(element, mes = '0') {
+    function setAdaptivePosition(element) {
       if (!state.initPosition.hasOwnProperty(element.name)) {
         state.initPosition[element.name] = [element.x, element.y]
       }  
-      console.log(mes)
       const heightScale = window.innerHeight / state.heightStart;
       const widthScale = window.innerWidth / state.widthStart;
       element.x = state.initPosition[element.name][0] * widthScale
@@ -50,166 +49,130 @@ export class Intro extends Phaser.Scene {
  
     // adaptive on SCREEN TURN
     const resizeListener = window.addEventListener('resize', function() {
-      setAdaptiveScale(soundButtonContainer);
+      // setAdaptiveScale(soundButtonContainer);
       setAdaptiveScale(girlTalkSprite);
       setAdaptiveScale(manTalkSprite);
       setAdaptiveScale(background);
 
-      setAdaptivePosition(soundButtonContainer)
+      // setAdaptivePosition(soundButtonContainer)
       setAdaptivePosition(girlTalkSprite)
-      setAdaptivePosition(manTalkSprite, 'man')
+      setAdaptivePosition(manTalkSprite)
       setAdaptivePosition(background)
     }, true);
 
-     // will MOVE selected element (move to 0.01 to delay)
-     function moveElementBy(element, x, y, seconds = 1) {
-      const moveElement = () => {
-        state.actions[element.type] = true;
+    // // SCALE element (live)
+    // function scaleElementBy(element, scaleBy, seconds = 1) {
+    //   const scaleElement = () => {
+    //     state.actions[element.type] = true;
 
-        const iterations = seconds * 1000 / 16;
-        const xStart = element.x;
-        const xEnd = xStart + x;
-        const yEnd = element.y + y;
+    //     const iterations = seconds * 1000 / 16;
+    //     const startScale = element.scale;
+    //     const endScale = element.scale * scaleBy;
+    //     let currentScale = startScale
+    //     const step = (endScale - startScale) / iterations;
 
-        const move = setInterval(() => {
-          element.x += x / iterations;
-          element.y += y / iterations;
-          const xInPlace = Math.abs(element.x - xEnd) <= Math.abs(x / iterations);
-          const yInPlace = Math.abs(element.y - yEnd) <= Math.abs(y / iterations);
+    //     const scale = setInterval(() => {
+    //       currentScale += step;
+    //       element.setScale(currentScale); 
 
-          if (xInPlace && yInPlace) {
-            element.x = xEnd;
-            clearInterval(move);
-            state.actions[element.type] = false;
-          }
-        }, 16);
-      };
+    //       const scaleDone = Math.abs(element.scale - endScale) <= Math.abs(step);
 
-      if (!state.actions.hasOwnProperty(element.type) || state.actions[element.type] === false) {
-        moveElement();
-      } else {
-        const wait = setInterval(() => {
-          if (state.actions[element.type] === false) {
-            moveElement();
-            clearInterval(wait);
-          }
-        }, 16);
-      } 
-    }
+    //       if (scaleDone) {
+    //         element.setScale(endScale);
+    //         clearInterval(scale);
+    //         state.actions[element.type] = false;
+    //       }
+    //     }, 16);
+    //   };
 
-    // SCALE element (live)
-    function scaleElementBy(element, scaleBy, seconds = 1) {
-      const scaleElement = () => {
-        state.actions[element.type] = true;
+    //   if (!state.actions.hasOwnProperty(element.type) || state.actions[element.type] === false) {
+    //     scaleElement();
+    //   } else {
+    //     const wait = setInterval(() => {
+    //       if (state.actions[element.type] === false) {
+    //         scaleElement();
+    //         clearInterval(wait);
+    //       }
+    //     }, 16);
+    //   } 
+    // }
 
-        const iterations = seconds * 1000 / 16;
-        const startScale = element.scale;
-        const endScale = element.scale * scaleBy;
-        let currentScale = startScale
-        const step = (endScale - startScale) / iterations;
+    //  // FADE element (live)
+    //  function fadeElementTo(element, fadeTo, seconds = 1) {
+    //   const fadeElement = () => {
+    //     state.actions[element.type] = true;
 
-        const scale = setInterval(() => {
-          currentScale += step;
-          element.setScale(currentScale); 
+    //     const iterations = seconds * 1000 / 16;
+    //     const startFade = element.alpha;
+    //     const endFade = fadeTo;
+    //     let currentFade = startFade;
+    //     const step = (endFade - startFade) / iterations;
 
-          const scaleDone = Math.abs(element.scale - endScale) <= Math.abs(step);
+    //     const fade = setInterval(() => {
+    //       currentFade += step;
+    //       element.setAlpha(currentFade); 
 
-          if (scaleDone) {
-            element.setScale(endScale);
-            clearInterval(scale);
-            state.actions[element.type] = false;
-          }
-        }, 16);
-      };
+    //       const fadeDone = Math.abs(element.alpha - endFade) <= Math.abs(step);
 
-      if (!state.actions.hasOwnProperty(element.type) || state.actions[element.type] === false) {
-        scaleElement();
-      } else {
-        const wait = setInterval(() => {
-          if (state.actions[element.type] === false) {
-            scaleElement();
-            clearInterval(wait);
-          }
-        }, 16);
-      } 
-    }
+    //       if (fadeDone) {
+    //         element.setAlpha(endFade);
+    //         clearInterval(fade);
+    //         state.actions[element.type] = false;
+    //       }
+    //     }, 16);
+    //   };
 
-     // FADE element (live)
-     function fadeElementTo(element, fadeTo, seconds = 1) {
-      const fadeElement = () => {
-        state.actions[element.type] = true;
+    //   if (!state.actions.hasOwnProperty(element.type) || state.actions[element.type] === false) {
+    //     fadeElement();
+    //   } else {
+    //     const wait = setInterval(() => {
+    //       if (state.actions[element.type] === false) {
+    //         fadeElement();
+    //         clearInterval(wait);
+    //       }
+    //     }, 16);
+    //   } 
+    // }
 
-        const iterations = seconds * 1000 / 16;
-        const startFade = element.alpha;
-        const endFade = fadeTo;
-        let currentFade = startFade;
-        const step = (endFade - startFade) / iterations;
-
-        const fade = setInterval(() => {
-          currentFade += step;
-          element.setAlpha(currentFade); 
-
-          const fadeDone = Math.abs(element.alpha - endFade) <= Math.abs(step);
-
-          if (fadeDone) {
-            element.setAlpha(endFade);
-            clearInterval(fade);
-            state.actions[element.type] = false;
-          }
-        }, 16);
-      };
-
-      if (!state.actions.hasOwnProperty(element.type) || state.actions[element.type] === false) {
-        fadeElement();
-      } else {
-        const wait = setInterval(() => {
-          if (state.actions[element.type] === false) {
-            fadeElement();
-            clearInterval(wait);
-          }
-        }, 16);
-      } 
-    }
-
-    // will keep track of TIMERS
-    function customTimer(callback, time) {
-      const timer = setTimeout(()=>{
-        callback();
-        state.timers.pop();
-      }, time);
-      state.timers.push(timer);
-    }
+    // // will keep track of TIMERS
+    // function customTimer(callback, time) {
+    //   const timer = setTimeout(()=>{
+    //     callback();
+    //     state.timers.pop();
+    //   }, time);
+    //   state.timers.push(timer);
+    // }
   
     // here we start to ADD things to the scene
 
-    // SOUND
-    this.sound.pauseOnBlur = false;  // will not stop in other window
-    let music = this.sound.add('music', {
-      loop: true
-    });
-    music.play();
+    // // SOUND
+    // this.sound.pauseOnBlur = false;  // will not stop in other window
+    // let music = this.sound.add('music', {
+    //   loop: true
+    // });
+    // music.play();
 
-    // Sound button container
-    const soundOn = this.add.image(0, 0, 'sound_on_button');
-    const soundOff = this.add.image(0, 0, 'sound_off_button').setAlpha(0);
-    const soundButtonContainer = this.add.container(window.innerWidth * 0.1, window.innerHeight * 0.9).setDepth(3);
-    soundButtonContainer.add(soundOn);
-    soundButtonContainer.add(soundOff);
-    const soundButtonScaleFactor = window.innerHeight / soundOn.height;
-    soundButtonContainer.setScale(0.1 * soundButtonScaleFactor);
-    soundOn.setInteractive();
-    soundOff.setInteractive();
+    // // Sound button container
+    // const soundOn = this.add.image(0, 0, 'sound_on_button');
+    // const soundOff = this.add.image(0, 0, 'sound_off_button').setAlpha(0);
+    // const soundButtonContainer = this.add.container(window.innerWidth * 0.1, window.innerHeight * 0.9).setDepth(3);
+    // soundButtonContainer.add(soundOn);
+    // soundButtonContainer.add(soundOff);
+    // const soundButtonScaleFactor = window.innerHeight / soundOn.height;
+    // soundButtonContainer.setScale(0.1 * soundButtonScaleFactor);
+    // soundOn.setInteractive();
+    // soundOff.setInteractive();
 
-    soundOn.on('pointerup', () => {
-      music.pause();
-      soundOff.setAlpha(100);
-      soundOn.setAlpha(0);
-    });
-    soundOff.on('pointerup', () => {
-      music.resume();
-      soundOff.setAlpha(0);
-      soundOn.setAlpha(100);
-    });
+    // soundOn.on('pointerup', () => {
+    //   music.pause();
+    //   soundOff.setAlpha(100);
+    //   soundOn.setAlpha(0);
+    // });
+    // soundOff.on('pointerup', () => {
+    //   music.resume();
+    //   soundOff.setAlpha(0);
+    //   soundOn.setAlpha(100);
+    // });
       
     // adaptive BACKGROUND
     const background = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'title_bg').setAlpha(0);
@@ -219,26 +182,26 @@ export class Intro extends Phaser.Scene {
     background.setScale(backgroundScale);
 
     // character SPRITE
-    const girlTalkSprite = this.add.sprite(window.innerWidth / 2, window.innerHeight / 2, 'girl-sprite');
+    const girlTalkSprite = this.add.sprite(window.innerWidth / 2 - 200, window.innerHeight / 2, 'girl-sprite');
 
     const characterScaleFactor = window.innerHeight / girlTalkSprite.height;
     girlTalkSprite.setScale(characterScaleFactor);
 
-    const manTalkSprite = this.add.sprite(window.innerWidth / 2, window.innerHeight / 2, 'man-sprite');
+    const manTalkSprite = this.add.sprite(window.innerWidth / 2 + 200, window.innerHeight / 2, 'man-sprite');
     manTalkSprite.setScale(characterScaleFactor);
 
-    // MESSAGE
-    // girl
-    const girlMessage = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'girl-message');
-    const girlMessageScaleFactor = window.innerHeight / soundOn.height;
-    girlMessage.setScale(0.7 * girlMessageScaleFactor);
-    girlMessage.setAlpha(0);
+    // // MESSAGE
+    // // girl
+    // const girlMessage = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'girl-message');
+    // const girlMessageScaleFactor = window.innerHeight / soundOn.height;
+    // girlMessage.setScale(0.7 * girlMessageScaleFactor);
+    // girlMessage.setAlpha(0);
 
-    // man
-    const manMessage = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'man-message');
-    const manMessageScaleFactor = window.innerHeight / soundOn.height;
-    manMessage.setScale(0.7 * manMessageScaleFactor);
-    manMessage.setAlpha(0);
+    // // man
+    // const manMessage = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'man-message');
+    // const manMessageScaleFactor = window.innerHeight / soundOn.height;
+    // manMessage.setScale(0.7 * manMessageScaleFactor);
+    // manMessage.setAlpha(0);
 
      // create ANIMATION
     this.anims.create({
@@ -259,61 +222,154 @@ export class Intro extends Phaser.Scene {
       }),
     });
 
-    // custom ANIMATIONS (character actions)
-    girlTalkSprite.x = - girlTalkSprite.width;
-    manTalkSprite.x = window.innerWidth + manTalkSprite.width;
+    // // custom ANIMATIONS (character actions)
+    // girlTalkSprite.x = - girlTalkSprite.width;
+    // manTalkSprite.x = window.innerWidth + manTalkSprite.width;
 
-    fadeElementTo(background, 0.5, 1);
-    fadeElementTo(background, 0.51, 6);
+    // fadeElementTo(background, 0.5, 1);
+    // fadeElementTo(background, 0.51, 6);
 
-    const charOneToCenter = manTalkSprite.x - window.innerWidth / 2
-    const charTwoToCenter = manTalkSprite.x - window.innerWidth / 2
-    moveElementBy(manTalkSprite, - charTwoToCenter, 0, 1);
+    // const charOneToCenter = manTalkSprite.x - window.innerWidth / 2
+    // const charTwoToCenter = manTalkSprite.x - window.innerWidth / 2
+    // moveElementBy(manTalkSprite, - charTwoToCenter, 0, 1);
 
-    customTimer(() => {
-      manMessage.setAlpha(1)
-    }, 1000);
-    customTimer(() => {
-      manMessage.setAlpha(0)
-    }, 3000);
-    customTimer(() => {
-      girlMessage.setAlpha(1)
-    }, 5000);
-    customTimer(() => {
-      girlMessage.setAlpha(0)
-    }, 7000);
+    // customTimer(() => {
+    //   manMessage.setAlpha(1)
+    // }, 1000);
+    // customTimer(() => {
+    //   manMessage.setAlpha(0)
+    // }, 3000);
+    // customTimer(() => {
+    //   girlMessage.setAlpha(1)
+    // }, 5000);
+    // customTimer(() => {
+    //   girlMessage.setAlpha(0)
+    // }, 7000);
 
 
     
-    manTalkSprite.play({
-      key: 'man-talk',
-      repeat: 4,
-      delay: 1000
-    });
-    moveElementBy(manTalkSprite, 0.1, 0, 2);
-    moveElementBy(manTalkSprite, charTwoToCenter, 0, 1);
+    // manTalkSprite.play({
+    //   key: 'man-talk',
+    //   repeat: 4,
+    //   delay: 1000
+    // });
+    // moveElementBy(manTalkSprite, 0.1, 0, 2);
+    // moveElementBy(manTalkSprite, charTwoToCenter, 0, 1);
 
-    moveElementBy(girlTalkSprite, charOneToCenter, 0, 1);
-    girlTalkSprite.play({
-      key: 'girl-talk',
-      repeat: 4,
-      delay: 5000
-    });
-    moveElementBy(girlTalkSprite, 0.1, 0, 2);
-    moveElementBy(girlTalkSprite, 0, 100, 1);
+    // moveElementBy(girlTalkSprite, charOneToCenter, 0, 1);
+    // girlTalkSprite.play({
+    //   key: 'girl-talk',
+    //   repeat: 4,
+    //   delay: 5000
+    // });
+    // moveElementBy(girlTalkSprite, 0.1, 0, 2);
+    // moveElementBy(girlTalkSprite, 0, 100, 1);
 
-    fadeElementTo(background, 1, 1);
+    // fadeElementTo(background, 1, 1);
 
 
     // giving names to element
-    manTalkSprite.name = 'manTalkSprite'; 
-    girlTalkSprite.name = 'girlTalkSprite'; 
-    background.name = 'background';
-    manMessage.name = 'manMessage';
-    girlMessage.name = 'manMessage';
-    soundButtonContainer.name = 'soundButtonContainer';
+    // manTalkSprite.name = 'manTalkSprite'; 
+    // girlTalkSprite.name = 'girlTalkSprite'; 
+    // background.name = 'background';
+    // manMessage.name = 'manMessage';
+    // girlMessage.name = 'manMessage';
+    // soundButtonContainer.name = 'soundButtonContainer';
     
-    
+    // setTimeout(()=> {
+    //   this.scene.start(CST.SCENES.FIRST_SCREEN);
+    // }, 8000)
+
+
+
+    girlTalkSprite.x = 100
+
+
+    function moveElementBy(element, moveX, moveY, seconds) {
+      return new Promise((res) => {
+        let iterations = Math.ceil(seconds * 1000 / 16);
+        const xStep = moveX / iterations;
+        const xEnd = element.x + moveX;
+        const yStep = moveY / iterations;
+        const yEnd = element.y + moveY;
+
+        const interval = setInterval(()=>{
+          element.x += xStep;
+          element.y += yStep;
+          iterations--;
+
+          if(iterations === 0) {
+            element.x = xEnd;
+            element.y = yEnd;
+            clearInterval(interval);
+            res('moveElementBy finished');
+          }
+        }, 16);
+      }) 
+    }
+
+    function scaleElementBy(element, scaleBy, seconds) {
+      return new Promise((res) => {
+        let iterations = Math.ceil(seconds * 1000 / 16);
+        const scaleEnd = element.scale * scaleBy;
+        const scaleStep = (scaleEnd - element.scale) / iterations;
+        let currentScale = element.scale
+
+        const interval = setInterval(()=>{
+          currentScale += scaleStep;
+          element.setScale(currentScale)
+          iterations--;
+
+          if(iterations === 0) {
+            element.setScale(scaleEnd);
+            clearInterval(interval);
+            res('scaleElementBy finished');
+          }
+        }, 16);
+      }) 
+    }
+
+
+
+    const moover2 = async() => {
+      scaleElementBy(girlTalkSprite, 1.5, 3);
+      scaleElementBy(manTalkSprite, 1.5, 3);
+      moveElementBy(manTalkSprite, 0, 150, 3);
+      await moveElementBy(girlTalkSprite, 0, 150, 3);
+    }
+
+    const moover3 = async() => {
+      await moveElementBy(manTalkSprite, -300, 0, 2);
+      await moveElementBy(manTalkSprite, 0, 0, 2);
+      await moveElementBy(manTalkSprite, 300, 0, 2);
+
+      await moveElementBy(girlTalkSprite, 300, 0, 2);
+      await moveElementBy(girlTalkSprite, 0, 0, 1);
+
+      await moover2();
+    }
+
+    moover3();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
 
   update() {
